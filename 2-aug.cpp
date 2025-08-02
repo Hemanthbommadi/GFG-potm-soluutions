93b25274179765538691aed85d933dd56c288dd3
Hemanthbommadi <bommadihemanth@gmail.com>
2025-08-02 20:14:14 +0530

    Add files via upload

---
+++ 2-aug.cpp
@@ -0,0 +1,45 @@
+
+class Solution
+{
+public:
+    int longestSubarray(vector<int> &arr, int k)
+    {
+        map<int, int> mp;
+        int sum = 0;
+        int n = arr.size();
+        int ans = 0;
+
+        for (int i = 0; i < n; i++)
+        {
+            int it = arr[i];
+
+            if (it > k)
+            {
+                sum++;
+            }
+            else
+            {
+                sum--;
+            }
+
+            if (sum > 0)
+            {
+                ans = i + 1;
+            }
+            else
+            {
+                if (mp.find(sum - 1) != mp.end())
+                {
+                    ans = max(ans, i - mp[sum - 1]);
+                }
+            }
+
+            if (mp.find(sum) == mp.end())
+            {
+                mp[sum] = i;
+            }
+        }
+
+        return ans;
+    }
+};
